139fc9f40799ccd99421bf8a449c8f13c83edc0b
Gonzalo <zalo250@gmail.com>
2015-02-25 22:23:20 +0000

    Added route functionality
    I have implemented the functionality for the route and the request to
    the api are working properly. I just have to show all the information
    in the view and then add the style.

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var bikeApp = angular.module('bikeApp', [
+  'ngRoute',
+  'bikeControllers'
+]);
+
+bikeApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/cities.html',
+        controller: 'CitiesCtrl'
+      }).
+      when('/details/:cityId', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ js/controllers.js
@@ -0,0 +1,15 @@
+var bikeControllers = angular.module('bikeControllers', []);
+
+bikeControllers.controller('CitiesCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('http://api.citybik.es/v2/networks').success(function(data){
+      $scope.places = data.networks;
+    });
+  }]);
+
+bikeControllers.controller('DetailsCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+	$http.get('http://api.citybik.es/v2/networks/' + $routeParams.cityId).success(function(data){
+      $scope.details = data.network.company;
+    });
+  }]);

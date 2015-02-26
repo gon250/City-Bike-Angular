faa8becf537a99c05cf8254f626030adef73e429
Gonzalo <zalo250@gmail.com>
2015-02-26 22:34:44 +0000

    fixing an issue with the route

--- js/require.config.js
+++ js/require.config.js
@@ -1,10 +1,10 @@
 requirejs.config({
     baseUrl: './',
     paths: {
-        angular: '../bower_components/angular/angular',
-        angularRoute: '../bower_components/angular-route/angular-route',
-        jquery: '../bower_components/jquery/dist/jquery.min',
-        bootstrap : "/bower_components/bootstrap/dist/js/bootstrap.min",
+        angular: './bower_components/angular/angular',
+        angularRoute: './bower_components/angular-route/angular-route',
+        jquery: './bower_components/jquery/dist/jquery.min',
+        bootstrap : "./bower_components/bootstrap/dist/js/bootstrap.min"
     },
     shim:{
         angularRoute:{
